64657fca42a8af60903cfb2a672f2a2fb8b2ce10
Ayush Aman <aayushamann@gmail.com>
2018-04-29 00:22:39 +0530

    Dialogs added

---
+++ app/src/main/java/com/saluchen/thisproject/ResponseDialog.java
@@ -0,0 +1,13 @@
+package com.saluchen.thisproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResponseDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.response_dialog);
+    }
+}
